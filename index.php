530f68307c971a30d4225561f1c2bd821d858b77
Gr4vity4 <prawared.bw@gmail.com>
2021-06-21 15:46:54 +0700

    remove fontawesome instead of svg

--- index.php
+++ index.php
@@ -60,26 +60,24 @@ add_action( 'rest_api_init', function () {
     ));
 });
 
-wp_enqueue_style('tailwind', '/wp-content/plugins/iot-interact/templates/assets/css/style.min.css');
-wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
-wp_enqueue_script('paho_mqtt', '/wp-content/plugins/iot-interact/mqtt/mqttws31.js');
+wp_enqueue_style('tailwind', '/wp-content/plugins/wp-netpie-plugin/templates/assets/css/style.min.css');
+wp_enqueue_script('paho_mqtt', '/wp-content/plugins/wp-netpie-plugin/mqtt/mqttws31.js');
 wp_enqueue_script('moment', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js');
 wp_enqueue_script('vue', 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js');
 
-add_filter('woocommerce_account_menu_items', 'iot_interact_dashboard_account_link', 40);
-function iot_interact_dashboard_account_link($menu_links)
+function netpie_dashboard_account_link($menu_links)
 {
     $menu_links = array_slice($menu_links, 0, 0, true)
-        + array('iot-interact-dashboard' => 'NETPIE')
+        + array('netpie-dashboard' => 'NETPIE')
         + array_slice($menu_links, 0, NULL, true);
 
     return $menu_links;
 }
+add_filter('woocommerce_account_menu_items', 'netpie_dashboard_account_link', 40);
 
-function iot_interact_my_account_endpoint_content()
+function netpie_my_account_endpoint_content()
 {
-    echo file_get_contents("wp-content/plugins/iot-interact/templates/dashboard.html");
+    echo file_get_contents("wp-content/plugins/wp-netpie-plugin/templates/dashboard.html");
 }
 
-add_action('woocommerce_account_iot-interact-dashboard_endpoint', 'iot_interact_my_account_endpoint_content');
-
+add_action('woocommerce_account_netpie-dashboard_endpoint', 'netpie_my_account_endpoint_content');
